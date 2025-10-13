aecc5bb512bb3d6513bbc3da146daaec21ab4055
HitoshiMiguel <miguelhitoshi@gmail.com>
2025-10-13 08:15:22 -0300

    feat: Adiciona ajustes do CSS e conversão do siteFatec e Consulta

---
+++ biblioteca-frontend/src/app/siteFatec/page.jsx
@@ -0,0 +1,40 @@
+// Arquivo: src/app/siteFatec/page.jsx
+'use client'; 
+
+import MainNavbar from "@/components/MainNavbar";
+import styles from './siteFatec.module.css';
+import { Container, Breadcrumb } from 'react-bootstrap';
+
+export default function SiteFatecPage() {
+  return (
+    <>
+      <MainNavbar className={styles.fatecNav} />
+      <div className={styles.breadcrumbWrap}>
+        <Container>
+          <Breadcrumb listProps={{ className: 'mb-0' }}>
+            <Breadcrumb.Item href="/">
+              <i className="bi bi-house-door-fill"></i> Home
+            </Breadcrumb.Item>
+            <Breadcrumb.Item active>
+                Informações da Biblioteca Online
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        </Container>
+      </div>
+
+      <main className={styles.mainContent}>
+        <h1 className={styles.pageTitle}>Biblioteca Online</h1>
+
+        <p className={styles.introText}>
+            A biblioteca da Fatec Zona Leste foi modernizada para oferecer mais praticidade, acessibilidade e eficiência no acesso às informações acadêmicas. Aqui, alunos e docentes encontram um espaço digital intuitivo, com recursos para consulta ao acervo, reserva de livros, acesso a obras digitais e materiais de estudo. Nosso objetivo é tornar o conhecimento ainda mais acessível, unindo tecnologia e educação em um ambiente confiável e fácil de usar.
+        </p>
+
+        <div className={styles.buttonContainer}>
+          <a href="/consulta" className={styles.actionButton}>Consulte o acervo online</a>
+          <a href="/solicitacao" className={styles.actionButton}>Solicitação para a biblioteca</a>
+        </div>
+
+        </main>
+    </>
+  );
+}
